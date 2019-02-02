c4db0faf7a5a013f516ace30205a2e351128d76b
Mitchell <mjacobsii0506@gmail.com>
2019-02-02 15:49:58 -0800

    add multipage, complete test

--- locators.py
+++ locators.py
@@ -11,8 +11,18 @@ class HomePageLocators:
 class SearchResultsPageLocators:
 	"""Class for search result page locators."""
 
+	APPLY_FILTERS = (By.XPATH, "//button[@data-rf-test-id='apply-search-options']")
 	MAX_BEDS = (By.XPATH, "//select[@name='maxBeds']")
+	MAX_FEET = (By.XPATH, "//select[@name='sqftMax']")
 	MAX_PRICE = (By.XPATH, "//select[@name='maxPrice']")
 	MIN_BEDS = (By.XPATH, "//select[@name='minBeds']")
+	MIN_FEET = (By.XPATH, "//select[@name='sqftMin']")
 	MIN_PRICE = (By.XPATH, "//select[@name='minPrice']")
 	MORE_FILTERS= (By.XPATH, "//button[@data-rf-test-id='filterButton']")
+	RESULT_ADDRESS = (By.XPATH, "//td[@class='column column_1 col_address']//a")
+	RESULT_BATHS = (By.XPATH, "//td[@class='column column_5 col_baths']")
+	RESULT_BEDS = (By.XPATH, "//td[@class='column column_4 col_beds']")
+	RESULT_SQ_FEET = (By.XPATH, "//td[@class='column column_6 col_sqft']")
+	RESULT_PRICE = (By.XPATH, "//td[@class='column column_3 col_price']")
+	RESULTS_LIST = (By.XPATH, "//tbody[@class='tableList']//tr")
+	RESULTS_TABLE = (By.XPATH, "//span[@data-rf-test-name='tableOption']")
